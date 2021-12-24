bd4563b269d2d2b941a5f7244441a6010cb2dccc
gomathi <tgomathi1806@gmail.com>
2021-12-24 15:05:53 +0000

    feet(*): added more tests

--- test/sample-test.js
+++ test/sample-test.js
@@ -71,18 +71,7 @@ describe('NFTMarket - After sales only one item should be left', function () {
 
     let items = await market.fetchMarketItems()
 
-    console.log('Number of Items after sales is 1', items.length)
-
-    items = await Promise.all(
-      items.map(async i => {
-        const tokenUri = await nft.tokenURI(i.tokenId)
-        let item = {
-          sold: i.sold.toString()
-        }
-        return item
-      })
-    )
-    console.log('items:', items)
+    expect(items).with.length(1)
   })
 })
 
@@ -121,18 +110,18 @@ describe('NFTMarket - After sales only one item should be left with sold as FALS
 
     let items = await market.fetchMarketItems()
 
-    console.log('Number of Items after sales', items.length)
-
     items = await Promise.all(
       items.map(async i => {
         const tokenUri = await nft.tokenURI(i.tokenId)
         let item = {
           sold: i.sold.toString()
         }
+        expect(item)
+          .to.have.property('sold')
+          .with.equal('false')
         return item
       })
     )
-    console.log('items:', items)
   })
 })
 
@@ -171,56 +160,31 @@ describe('NFTMarket - After sales only one item should be left And its tokenId s
 
     let items = await market.fetchMarketItems()
 
-    console.log('Number of Items after sales', items.length)
-
     items = await Promise.all(
       items.map(async i => {
         const tokenUri = await nft.tokenURI(i.tokenId)
         let item = {
-          tokenId: i.tokenId.toString()
+          tokenId: i.tokenId
         }
+        expect(item)
+          .to.have.property('tokenId')
+          .to.equal(2)
         return item
       })
     )
-    console.log('items:', items)
   })
 })
 
-describe('NFTMarket - After sales only one item should be left', function () {
-  it('Only one item should be left', async function () {
+describe('NFTMarket - Should create and execute market sales', function () {
+  it('Should deploy marketsales Contract', async function () {
     const Market = await ethers.getContractFactory('NFTMarket')
     const market = await Market.deploy()
     await market.deployed()
     const marketAddress = market.address
 
-    const NFT = await ethers.getContractFactory('NFT')
-    const nft = await NFT.deploy(marketAddress)
-    await nft.deployed()
-    const nftContractAddress = nft.address
-
-    let listingPrice = await market.getListingPrice()
-    listingPrice = listingPrice.toString()
-
-    const auctionPrice = ethers.utils.parseUnits('100', 'ether')
-
-    await nft.createToken('https://www.mytokenlocation.com')
-    await nft.createToken('https://www.mytokenlocation2.com')
-
-    await market.createMarketItem(nftContractAddress, 1, auctionPrice, {
-      value: listingPrice
-    })
-    await market.createMarketItem(nftContractAddress, 2, auctionPrice, {
-      value: listingPrice
-    })
-
-    const [_, buyerAddress] = await ethers.getSigners()
-
-    await market
-      .connect(buyerAddress)
-      .createMarketSale(nftContractAddress, 1, { value: auctionPrice })
-
-    let items = await market.fetchMarketItems()
-
-    console.log('Number of Items after sales', items.length)
+    expect(marketAddress).to.not.equal(0x0)
+    expect(marketAddress).to.not.equal('')
+    expect(marketAddress).to.not.equal(null)
+    expect(marketAddress).to.not.equal(undefined)
   })
 })
